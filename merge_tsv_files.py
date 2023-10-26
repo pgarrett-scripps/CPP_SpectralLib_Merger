9dfc1c3a1dd569992b5c1aa6c6d06e08d677b1c1
pgarrett <pgarrett@scripps.edu>
2023-10-26 09:37:58 -0700

    improved memory footprint for large files, added option to filter non lysine containing peptides, and duplicated peptides

--- merge_tsv_files.py
+++ merge_tsv_files.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import tkinter as tk
 from tkinter import filedialog
 
@@ -8,30 +7,49 @@ def merge_tsv_files():
     root.withdraw()
 
     # Ask the user to select multiple TSV files
-    file_paths = filedialog.askopenfilenames(filetypes=[("TSV Files", "*.tsv")])
+    file_paths = filedialog.askopenfilenames(filetypes=[("TSV Files", "*.tsv")], multiple=True, title="Select Input TSV Files")
     if not file_paths:
         print("No files selected. Exiting.")
         return
 
-    # Initialize an empty list to store DataFrames
-    dfs = []
-
-    # Read each selected TSV file into a DataFrame and append it to the list
-    for file_path in file_paths:
-        df = pd.read_csv(file_path, sep='\t')
-        dfs.append(df)
-
-    # Merge the DataFrames (assuming they have the same columns)
-    merged_df = pd.concat(dfs, ignore_index=True)
-
     # Ask the user to choose a location to save the merged TSV file
-    save_path = filedialog.asksaveasfilename(defaultextension=".tsv", filetypes=[("TSV Files", "*.tsv")])
+    save_path = filedialog.asksaveasfilename(defaultextension=".tsv", filetypes=[("TSV Files", "*.tsv")], title="Save Merged TSV File")
     if not save_path:
         print("No save location selected. Exiting.")
         return
 
-    # Save the merged DataFrame to the selected location
-    merged_df.to_csv(save_path, sep='\t', index=False)
+    seen = set()
+    header_written = False
+
+    with open(save_path, 'w') as of:
+        for file_path in file_paths:
+            with open(file_path, 'r') as f:
+                curr_peptide_charge = None
+                for i, line in enumerate(f):
+                    if i == 0:
+                        # Write the header line only once
+                        if not header_written:
+                            of.write(line)
+                            header_written = True
+                        continue
+
+                    line_elements = line.strip().split('\t')
+                    peptide, charge, unmod_peptide = line_elements[0], int(line_elements[1]), line_elements[4]
+
+                    # Check if we have encountered a new peptide and charge combination
+                    if curr_peptide_charge and curr_peptide_charge != (peptide, charge):
+                        seen.add(curr_peptide_charge)
+                        curr_peptide_charge = (peptide, charge)
+                    elif not curr_peptide_charge:
+                        curr_peptide_charge = (peptide, charge)
+
+                    # Check for 'K' in unmod_peptide and if (peptide, charge) is not in seen
+                    if 'K' in unmod_peptide and curr_peptide_charge not in seen:
+                        of.write(line)
+
+                # Add the last encountered peptide and charge to seen
+                if curr_peptide_charge:
+                    seen.add(curr_peptide_charge)
 
     print(f"Merged TSV file saved to: {save_path}")
 
